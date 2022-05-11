09b7644f07a5fdefb471b45fc87a45ab9ad993dc
zzyyppqq <zzyyppqq@gmail.com>
2022-05-11 15:07:35 +0800

    修改ffmpeg.c文件，ffmpeg.c本身是用于命令行使用的程序，因此在改为类库调用的时候需要对源代码做一些修改。例如去掉一些exit(0)，释放一些变量，main()函数改名为ffmpegmain()

--- libffmpeg/libs/include/ffmpeg.c
+++ libffmpeg/libs/include/ffmpeg.c
@@ -4788,8 +4788,7 @@ static void log_callback_null(void *ptr, int level, const char *fmt, va_list vl)
 {
 }
 
-int ffmpegmain(int argc, char **argv)
-{
+int ffmpegmain(int argc, char **argv) {
     int i, ret;
     int64_t ti;
 
@@ -4797,13 +4796,13 @@ int ffmpegmain(int argc, char **argv)
 
     register_exit(ffmpeg_cleanup);
 
-    setvbuf(stderr,NULL,_IONBF,0); /* win32 runtime needs this */
+    setvbuf(stderr, NULL, _IONBF, 0); /* win32 runtime needs this */
 
     av_log_set_flags(AV_LOG_SKIP_REPEATED);
     parse_loglevel(argc, argv, options);
 
-    if(argc>1 && !strcmp(argv[1], "-d")){
-        run_as_daemon=1;
+    if (argc > 1 && !strcmp(argv[1], "-d")) {
+        run_as_daemon = 1;
         av_log_set_callback(log_callback_null);
         argc--;
         argv++;
@@ -4818,19 +4817,27 @@ int ffmpegmain(int argc, char **argv)
 
     /* parse options and open all input/output files */
     ret = ffmpeg_parse_options(argc, argv);
-    if (ret < 0)
-        exit_program(1);
+    if (ret < 0) {
+        //exit_program(1);
+        ffmpeg_cleanup(1);
+        return 1;
+    }
 
     if (nb_output_files <= 0 && nb_input_files == 0) {
         show_usage();
-        av_log(NULL, AV_LOG_WARNING, "Use -h to get full help or, even better, run 'man %s'\n", program_name);
-        exit_program(1);
+        av_log(NULL, AV_LOG_WARNING, "Use -h to get full help or, even better, run 'man %s'\n",
+               program_name);
+        //exit_program(1);
+        ffmpeg_cleanup(1);
+        return 1;
     }
 
     /* file converter / grab */
     if (nb_output_files <= 0) {
         av_log(NULL, AV_LOG_FATAL, "At least one output file must be specified\n");
-        exit_program(1);
+        //exit_program(1);
+        ffmpeg_cleanup(1);
+        return 1;
     }
 
 //     if (nb_input_files == 0) {
@@ -4844,17 +4851,21 @@ int ffmpegmain(int argc, char **argv)
     }
 
     current_time = ti = getutime();
-    if (transcode() < 0)
-        exit_program(1);
+    if (transcode() < 0) {
+        //exit_program(1);
+        ffmpeg_cleanup(1);
+        return 1;
+    }
     ti = getutime() - ti;
     if (do_benchmark) {
         av_log(NULL, AV_LOG_INFO, "bench: utime=%0.3fs\n", ti / 1000000.0);
     }
     av_log(NULL, AV_LOG_DEBUG, "%"PRIu64" frames successfully decoded, %"PRIu64" decoding errors\n",
            decode_error_stat[0], decode_error_stat[1]);
-    if ((decode_error_stat[0] + decode_error_stat[1]) * max_error_rate < decode_error_stat[1])
-        exit_program(69);
-
-    exit_program(received_nb_signals ? 255 : main_return_code);
+//    if ((decode_error_stat[0] + decode_error_stat[1]) * max_error_rate < decode_error_stat[1]) {
+//        exit_program(69);
+//    }
+    //exit_program(received_nb_signals ? 255 : main_return_code);
+    ffmpeg_cleanup(0);
     return main_return_code;
 }
